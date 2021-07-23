7f853bcdff1c4bf75ee973b484bca71a1769ff8d
Adrian <adrianmouzinhopro@gmail.com>
2021-07-23 11:27:14 -0300

    Aula 03 - depoimentos e contato

--- main.js
+++ main.js
@@ -30,3 +30,29 @@ window.addEventListener('scroll', function() {
         header.classList.remove('scroll')
     }
 })
+
+/* testimonials carrossel slide swipper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* ScrollReveal: Mostrar elementos quando ser scroll na página */
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '35px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonial,
+    #contact .text, #contact .info
+    `, { interval: 100 })
